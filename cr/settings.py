4f87586234dc4abac8c01f9941ef5e1b4100dcad
mochatek <akashsabu@ymail.com>
2019-10-12 18:58:40 +0530

    Final

--- cr/settings.py
+++ cr/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY','#p#6hu1%*3hmaq$$ozjlv_g2hwkasx0^tz4bpxa7-9)=n*m-8v')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    'leaderoyale.herokuapp.com'
+    'https://leaderoyale.herokuapp.com'
 ]
 
 
