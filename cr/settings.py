b4e9b02576c4431f8f3e0a680c23e433ffe021c0
mochatek <akashsabu@ymail.com>
2019-10-12 18:42:05 +0530

    debug set false

--- cr/settings.py
+++ cr/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY','#p#6hu1%*3hmaq$$ozjlv_g2hwkasx0^tz4bpxa7-9)=n*m-8v')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
